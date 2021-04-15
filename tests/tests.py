17164f2804c2d4d709ee30705556b819935c2b63
virbickt <63246400+virbickt@users.noreply.github.com>
2021-04-15 16:24:37 +0300

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -3,75 +3,60 @@ from utils.input_processor import process_input
 
 
 def validate_input_number_of_rooms():
-  test_inputs = {
-            [
+  test_inputs = 
                 {
                 "area": 31,
                 "floor_on": 5,
                 "floors_total": 9,
                 "district": "Antakalnis"
                 }
-            ]
-          }
   with pytest.raises(ValueError):
     validate_inputs(test_inputs)
 
 
 def validate_input_area():
-  test_inputs = {
-            [
+  test_inputs = 
                 {
                 "number_of_rooms": 1,
                 "floor_on": 5,
                 "floors_total": 9,
                 "district": "Antakalnis"
                 }
-            ]
-          }
   with pytest.raises(ValueError):
     validate_inputs(test_inputs)
 
 
 def validate_input_floor_on():
-  test_inputs = {
-            [
+  test_inputs = 
                 {
                 "area": 31,
                 "number_of_rooms": 1,
                 "floors_total": 9,
                 "district": "Antakalnis"
                 }
-            ]
-          }
   with pytest.raises(ValueError):
     validate_inputs(test_inputs)
 
 
 def validate_input_floors_total():
-  test_inputs = {
-            [
+  test_inputs = 
                 {
                 "area": 31,
                 "number_of_rooms": 1,
                 "floor_on": 5,
                 "district": "Antakalnis"
                 }
-            ]
-          }
   with pytest.raises(ValueError):
     validate_inputs(test_inputs)
 
 def validate_input_district():
-  test_inputs = {
-            [
+  test_inputs = 
               {
               "number_of_rooms": 1,
               "area": 31,
               "floor_on": 5,
               "floors_total": 9
               }
-           ]
-          }
   with pytest.raises(ValueError):
     assert validate_inputs(test_inputs)
 
